0ba7502ecede299fe53eaf6d81c97e03d03fc431
jpllosa <jopeellosa@gmail.com>
2018-09-17 10:53:25 +0100

    added close statement

--- src/main/java/joel/llosa/Main.java
+++ src/main/java/joel/llosa/Main.java
@@ -1,14 +1,13 @@
 package joel.llosa;
 
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
 
 	public static void main(String[] args) {
-		ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
 		HiSpring hs = (HiSpring) context.getBean("hiSpring");
 		hs.getMessage();
+		context.close();
 	}
-
 }
